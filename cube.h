684f6a684608a6ae753bfd6182d0149fd3e61522
Sophia van Valkenburg <sophia.vanvalkenburg@cbre.com>
2017-09-18 23:30:43 -0400

    create CubeFace class and fill in initFaces and getFaceCenter methods

--- cube.h
+++ cube.h
@@ -2,19 +2,75 @@
 #define CUBE_H
 
 #include <vector>
+#include <map>
 #include <glm/glm.hpp>
 
 class SubCube
 {
-    glm::vec3 position;
+    private:
+        unsigned int id;
+        glm::vec3 position;
     public:
         SubCube() = default;
-        SubCube(glm::vec3 position){
+        SubCube(glm::vec3 position, unsigned int id){
             this->position = position;
+            this->id = id;
         }
 
         glm::vec3 getPosition(){
             return this->position;
+        }
+
+        unsigned int getId(){
+            return this->id;
+        }
+};
+
+class CubeFace
+{
+    private:
+        std::vector<unsigned int> subcubeIds;
+        glm::vec3 center;
+
+        float getPositionAverage(float centerPosition, float newPosition, int numSubCubes){
+            return (numSubCubes * centerPosition + newPosition) / (numSubCubes + 1);
+        }
+
+    public:
+        CubeFace() = default;
+
+        unsigned int getSize(){
+            return this->subcubeIds.size();
+        }
+
+        glm::vec3 getCenter(){
+            return this->center;
+        }
+
+        bool containsSubCube(unsigned int subcubeId){
+            for (unsigned int i=0; i<this->subcubeIds.size(); i++){
+                if (this->subcubeIds[i] == subcubeId) return true;
+            }
+            return false;
+        }
+
+        void addSubCube(SubCube s){
+            unsigned int numSubCubes = this->subcubeIds.size();
+            glm::vec3 subcubePos = s.getPosition();
+            float centerX = getPositionAverage(this->center.x, subcubePos.x, numSubCubes);
+            float centerY = getPositionAverage(this->center.y, subcubePos.y, numSubCubes);
+            float centerZ = getPositionAverage(this->center.z, subcubePos.z, numSubCubes);
+            this->center.x = centerX;
+            this->center.y = centerY;
+            this->center.z = centerZ;
+            this->subcubeIds.push_back(s.getId());
+        }
+
+        void print(){
+            for (unsigned int i=0; i<this->subcubeIds.size(); i++){
+                std::cout << this->subcubeIds[i] << " ";
+            }
+            std::cout << std::endl;
         }
 };
 
@@ -70,11 +126,9 @@ class Cube
         -0.15f,  0.15f, -0.15f,  1.0f, 1.0f, 0.0f
     };
 
-    static const int NUM_SUBCUBES = 27;
-
     std::vector<SubCube> subcubes;
 
-    std::vector<std::vector<unsigned int>> faces;
+    std::vector<CubeFace> faces;
     
     public:
         Cube(){
@@ -89,29 +143,50 @@ class Cube
             return sizeof(this->vertices);
         }
 
-        unsigned int getNumPositions(){
-            return NUM_SUBCUBES;
-        }
-
         std::vector<SubCube> getSubCubes(){
             return this->subcubes;
         }
 
         bool faceContainsSubCube(unsigned int faceId, unsigned int subcubeId){
-            for (unsigned int i=0; i<9; i++){
-                if (this->faces[0][i] == subcubeId) return true;
-            }
-            return false;
+            return this->faces[faceId].containsSubCube(subcubeId);
         }
 
         glm::vec3 getFaceCenter(unsigned int faceId){
-            return glm::vec3(-0.325f, 0.0f, 0.0f);
+            return this->faces[faceId].getCenter();
         }
+
+        void printFaces(){
+            for (unsigned int i=0; i<this->faces.size(); i++){
+                this->faces[i].print();
+            }
+        }
+
     private:
+        void addSubCubeToFace(std::map<float, unsigned int> &posMap, float posCoord, SubCube s){
+            unsigned int faceIndex = posMap[posCoord];
+            if (faceIndex == 0){
+                faceIndex = this->faces.size() + 1; // + 1 because we need to reserve 0 for checking map
+                posMap[posCoord] = faceIndex;                    
+                CubeFace cubeface;
+                this->faces.push_back(cubeface);
+            }
+            CubeFace cubeface = this->faces[faceIndex - 1];
+            cubeface.addSubCube(s);
+            this->faces[faceIndex - 1] = cubeface;
+        }
         void initFaces(){
-            this->faces = {
-                {2, 6, 8, 11, 15, 17, 20, 24, 26}
-            };
+            // construct the faces by grouping together all subcubes that share an X, Y, or Z value
+            std::map<float, unsigned int> xPosToIndexMap;
+            std::map<float, unsigned int> yPosToIndexMap;
+            std::map<float, unsigned int> zPosToIndexMap;
+            for (unsigned int i=0; i<this->subcubes.size(); i++){
+                SubCube s = this->subcubes[i];
+                glm::vec3 pos = s.getPosition();
+                addSubCubeToFace(xPosToIndexMap, pos.x, s);
+                addSubCubeToFace(yPosToIndexMap, pos.y, s);
+                addSubCubeToFace(zPosToIndexMap, pos.z, s);
+            }
+            printFaces();
         }
         void initSubCubes(){
             std::vector<glm::vec3> positions = {
@@ -144,7 +219,7 @@ class Cube
                 glm::vec3(-0.325f, -0.325f, -0.325f)
             };
             for (unsigned int i=0; i<positions.size(); i++){
-                this->subcubes.push_back(SubCube(positions[i]));
+                this->subcubes.push_back(SubCube(positions[i], i));
             }
         }
 };
