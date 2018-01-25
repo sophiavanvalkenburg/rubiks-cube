e0299837a9a536791b8d66046a901ca554b59b5b
Sophia van Valkenburg <sophia.vanvalkenburg@cbre.com>
2018-01-24 19:52:18 -0500

    only move face when mouse is down

--- InputHandler.h
+++ InputHandler.h
@@ -77,31 +77,32 @@ void mouseCallback(GLFWwindow* window, double xpos, double ypos){
     State::mouseY = ypos;
 
     if (State::mouseBtnIsDown){
-        if (State::cubeFirstMouse)
-        {
+        if (State::faceRotationBtnIsDown){
+            if (State::faceFirstMouse){
+                State::faceLastY = ypos;
+                State::faceFirstMouse = false;
+            }
+            float yoffset = calculatePositionOffset(ypos, State::faceLastY);
+            State::faceLastY = ypos;
+            State::faceRotationAngle += glm::radians(yoffset);
+        } else {
+            if (State::cubeFirstMouse)
+            {
+                State::cubeLastX = xpos;
+                State::cubeLastY = ypos;
+                State::cubeFirstMouse = false;
+            }
+
+            float xoffset = calculatePositionOffset(xpos, State::cubeLastX);
+            float yoffset = calculatePositionOffset(ypos, State::cubeLastY);
+
             State::cubeLastX = xpos;
             State::cubeLastY = ypos;
-            State::cubeFirstMouse = false;
+
+            State::rubiksCube.yawAngle = glm::radians(xoffset); 
+            State::rubiksCube.pitchAngle = glm::radians(yoffset);
         }
-
-        float xoffset = calculatePositionOffset(xpos, State::cubeLastX);
-        float yoffset = calculatePositionOffset(ypos, State::cubeLastY);
-
-        State::cubeLastX = xpos;
-        State::cubeLastY = ypos;
-
-        State::rubiksCube.yawAngle = glm::radians(xoffset); 
-        State::rubiksCube.pitchAngle = glm::radians(yoffset);
-
-    } else if (State::faceRotationBtnIsDown){
-       if (State::faceFirstMouse){
-           State::faceLastY = ypos;
-           State::faceFirstMouse = false;
-       }
-       float yoffset = calculatePositionOffset(ypos, State::faceLastY);
-       State::faceLastY = ypos;
-       State::faceRotationAngle += glm::radians(yoffset);
-    }
+    } 
 }
 
 #endif
